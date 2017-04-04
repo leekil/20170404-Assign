95a1e4ba9cb02d3f1df4cb7971a89a3dae83ab81
leekil <dlrlf369@naver.com>
2017-04-04 14:59:26 +0900

    circle
    circle 소스파일

---
+++ javaOO/Circle.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Circle {
+	public double radius;
+	public static double PI=3.141592;
+
+	public Circle(double radius){
+		this.radius=radius;
+	}
+	public double getArea(){
+		return radius * radius * PI;
+	}
+}
